988f098abddd8c4cef7d93675bc3c05bccda5b92
Pablo <PabloAsekas@users.noreply.github.com>
2021-10-13 22:39:48 +0200

    feat: added compatibility to create new WhatsAppMedia objects

--- src/OrderMedia/Services/MediaFactoryService.cs
+++ src/OrderMedia/Services/MediaFactoryService.cs
@@ -1,5 +1,5 @@
-﻿// <copyright file="MediaFactoryService.cs" company="PlaceholderCompany">
-// Copyright (c) PlaceholderCompany. All rights reserved.
+﻿// <copyright file="MediaFactoryService.cs" company="Pablo Bermejo">
+// Copyright (c) Pablo Bermejo. All rights reserved.
 // </copyright>
 
 namespace OrderMedia.Services
@@ -33,6 +33,10 @@ namespace OrderMedia.Services
         public BaseMedia CreateMedia(string path)
         {
             string extension = Path.GetExtension(path);
+            string name = Path.GetFileNameWithoutExtension(path);
+
+            extension = this.AnalyzeMediaName(name, "PHOTO", ".WhatsAppImage", extension);
+            extension = this.AnalyzeMediaName(name, "VIDEO", ".WhatsAppVideo", extension);
 
             return extension.ToLower() switch
             {
@@ -43,8 +47,23 @@ namespace OrderMedia.Services
                 ".png" => new PngMedia(path, this.configurationService.GetImageFolderName()),
                 ".mov" => new MovMedia(path, this.configurationService.GetVideoFolderName()),
                 ".mp4" => new Mp4Media(path, this.configurationService.GetVideoFolderName()),
+                ".whatsappimage" => new WhatsAppMedia(path, this.configurationService.GetImageFolderName()),
+                ".whatsappvideo" => new WhatsAppMedia(path, this.configurationService.GetVideoFolderName()),
                 _ => throw new FormatException($"The provided extension '{extension.ToLower()}' is not supported."),
             };
+        }
+
+        /// <summary>
+        /// Modify the extension if the media name starts with the given string.
+        /// </summary>
+        /// <param name="name">Media name.</param>
+        /// <param name="startsWith">String that will be compared.</param>
+        /// <param name="newExtension">New extension if the name starts with the given string.</param>
+        /// <param name="oldExtension">Old extension that will be returned if the name doesn't start with the given string.</param>
+        /// <returns>The new extension if the name starts with the given string. The old extension if the name doesn't.</returns>
+        private string AnalyzeMediaName(string name, string startsWith, string newExtension, string oldExtension)
+        {
+            return name.StartsWith(startsWith) ? newExtension : oldExtension;
         }
     }
 }
