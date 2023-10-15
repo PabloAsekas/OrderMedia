640aee2bef6a7039261ca302c8389babd61b4ea0
Pablo <pablo@mylaif.es>
2023-10-15 20:04:53 +0200

    Fixed error while getting the name of the aae file for media files with small name.

--- src/OrderMediaTests/Services/RenameServiceTests.cs
+++ src/OrderMediaTests/Services/RenameServiceTests.cs
@@ -61,6 +61,7 @@ namespace OrderMediaTests.Services
         [Test]
 		[TestCase("IMG_0001", "IMG_O0001.aae")]
         [TestCase("IMG_0001 (1)", "IMG_0001 (1)O.aae")]
+        [TestCase("Test", "Test.aae")]
         public void GetAaeName_Returns_AaeName_Successfully(string nameWithoutExtension, string aaeName)
 		{
 			// Arrange
