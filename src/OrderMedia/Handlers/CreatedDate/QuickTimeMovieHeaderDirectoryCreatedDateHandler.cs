af8f034b7fccbfd18ff29efbf2daabf63b08c17b
Pablo <pablo@mylaif.es>
2024-08-15 11:14:37 +0200

    Added QuickTimeMovieHeaderDirectory Handler

---
+++ src/OrderMedia/Handlers/CreatedDate/QuickTimeMovieHeaderDirectoryCreatedDateHandler.cs
@@ -0,0 +1,24 @@
+using MetadataExtractor.Formats.QuickTime;
+using OrderMedia.Interfaces;
+using OrderMedia.Models;
+
+namespace OrderMedia.Handlers.CreatedDate;
+
+public class QuickTimeMovieHeaderDirectoryCreatedDateHandler: BaseCreatedDateHandler
+{
+    private readonly IImageMetadataReader _imageMetadataReader;
+
+    public QuickTimeMovieHeaderDirectoryCreatedDateHandler(IImageMetadataReader imageMetadataReader)
+    {
+        _imageMetadataReader = imageMetadataReader;
+    }
+        
+    public override CreatedDateInfo GetCreatedDateInfo(string mediaPath)
+    {
+        var createdDate = _imageMetadataReader.GetMetadataByDirectoryTypeAndTag<QuickTimeMovieHeaderDirectory>(mediaPath, QuickTimeMovieHeaderDirectory.TagCreated);
+
+        var createdDateInfo = CreateCreatedDateInfo(createdDate, "ddd MMM dd HH:mm:ss yyyy");
+            
+        return createdDateInfo ?? base.GetCreatedDateInfo(mediaPath);
+    }
+}
